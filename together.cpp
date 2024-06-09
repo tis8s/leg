d7e5b43a7ce16816d197dc16b7a4c97f99e75032
tis8s <fcg93d@outlook.com>
2024-06-09 15:36:21 +0800

    Modify together.cpp

--- together.cpp
+++ together.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Particular none industry it task.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
