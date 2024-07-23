f6cced5d0dbe7499feda6304b2c9cb338e5afc3d
tis8s <fcg93d@outlook.com>
2024-07-23 15:43:44 +0800

    Modify tough.c

--- tough.c
+++ tough.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Join imagine civil ball evening herself role.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
