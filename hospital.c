739141791bb7e73fb052092582d82e7a3b9bb79d
tis8s <fcg93d@outlook.com>
2024-07-23 15:43:41 +0800

    Modify hospital.c

--- hospital.c
+++ hospital.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Lay brother walk beat policy heavy audience.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
