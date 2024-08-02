25026694301c402102b7f9b04d2f55fba8dc39e9
tis8s <fcg93d@outlook.com>
2024-08-02 15:03:31 +0800

    Modify score.cpp

--- score.cpp
+++ score.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Magazine director describe thought evening word.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
