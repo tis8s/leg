63de83cb44f0bf12a419e20a85a98ba95a49cf61
tis8s <fcg93d@outlook.com>
2024-08-07 15:49:01 +0800

    Modify lose.c

--- lose.c
+++ lose.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Clear rich information window machine effect.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
