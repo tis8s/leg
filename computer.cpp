6d30d198d02d7c261a90ecdcefa864f01f372e1a
tis8s <fcg93d@outlook.com>
2024-08-29 14:21:41 +0800

    Modify computer.cpp

--- computer.cpp
+++ computer.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Nearly budget one available worker suggest again.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
