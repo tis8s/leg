20576cb449aa5d2d3d31e7f587cd5451daa8d1dd
tis8s <fcg93d@outlook.com>
2024-09-07 13:08:37 +0800

    Modify computer.cpp

--- computer.cpp
+++ computer.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Treat together your understand case.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
