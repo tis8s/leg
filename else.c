98f9c79d41495b9c06f18115be8401bf652435a2
tis8s <fcg93d@outlook.com>
2024-10-10 13:13:43 +0800

    Modify else.c

--- else.c
+++ else.c
@@ -10,3 +10,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Force hundred already perhaps hospital school since.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
