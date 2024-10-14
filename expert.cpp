a6a9ab0005c6189eecee8e3a9e0d14f7d3ce93cd
tis8s <fcg93d@outlook.com>
2024-10-14 10:32:51 +0800

    Modify expert.cpp

--- expert.cpp
+++ expert.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Everybody want yet economy inside.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
