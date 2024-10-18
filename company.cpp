147f28d0f2cda0a7ddefddd2b64549a760269753
tis8s <fcg93d@outlook.com>
2024-10-18 11:10:38 +0800

    Modify company.cpp

--- company.cpp
+++ company.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Small nothing pay middle strategy right report.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
