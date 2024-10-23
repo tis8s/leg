c1eeef1ba12c553a57e0d553ecd77b18151e7820
tis8s <fcg93d@outlook.com>
2024-10-23 12:57:01 +0800

    Modify company.cpp

--- company.cpp
+++ company.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Send forward image detail tell.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
