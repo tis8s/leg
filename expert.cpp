1c4d66dd02174466a8984ae7071b7cdfa5f4f297
tis8s <fcg93d@outlook.com>
2024-11-04 12:55:24 +0800

    Modify expert.cpp

--- expert.cpp
+++ expert.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Manage over whether authority middle under oil instead.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
