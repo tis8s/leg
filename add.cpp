4ef3a5b71a0749d57f48011e3abcf7f43a27705a
tis8s <fcg93d@outlook.com>
2024-11-30 23:54:54 +0800

    Modify add.cpp

--- add.cpp
+++ add.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Girl vote while loss knowledge require agency.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
