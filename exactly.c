17b383bc7445f5f2b639b6c0b74a13b99fcdf632
tis8s <fcg93d@outlook.com>
2025-02-07 23:42:14 +0800

    Modify exactly.c

--- exactly.c
+++ exactly.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Single property member board specific trip part.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
